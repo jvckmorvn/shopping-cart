3dc1204eea7dcf661d7e10e3f7773e591e09192c
Jack Moran <jvckmorvn@gmail.com>
2022-07-16 21:19:15 +0100

    Implemented decrement button

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,27 +3,43 @@ import React, { Component } from "react";
 class Counter extends Component {
   render() {
     return (
-      <div>
-        <span className={this.getButtonClasses()}>{this.formatValue()}</span>
-        <button
-          onClick={() => this.props.onIncrement(this.props.counter)}
-          className="btn btn-secondary btn-sm"
-        >
-          Increment
-        </button>
-        <button
-          className="btn btn-danger btn-sm m-2"
-          onClick={() => this.props.onDelete(this.props.counter.id)}
-        >
-          Delete
-        </button>
+      <div className="row">
+        <div className="col-1">
+          <span className={this.getCounterClasses()}>{this.formatValue()}</span>
+        </div>
+        <div className="col">
+          <button
+            onClick={() => this.props.onIncrement(this.props.counter)}
+            className={this.getButtonClasses()}
+          >
+            +
+          </button>
+          <button
+            onClick={() => this.props.onDecrement(this.props.counter)}
+            className={this.getButtonClasses()}
+            disabled={this.props.counter.value === 0 ? "disabled" : ""}
+          >
+            -
+          </button>
+          <button
+            className="btn btn-danger btn-sm m-2"
+            onClick={() => this.props.onDelete(this.props.counter.id)}
+          >
+            x
+          </button>
+        </div>
       </div>
     );
   }
 
-  getButtonClasses() {
+  getCounterClasses() {
     let classes = "badge m-2 bg-";
     classes += this.props.counter.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  getButtonClasses() {
+    const classes = "btn btn-secondary btn-sm m-2";
     return classes;
   }
 
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,7 +3,8 @@ import Counter from "./counter";
 
 class Counters extends Component {
   render() {
-    const { onReset, counters, onDelete, onIncrement } = this.props;
+    const { onReset, counters, onDelete, onIncrement, onDecrement } =
+      this.props;
     return (
       <div>
         {counters.map((counter) => (
@@ -11,6 +12,7 @@ class Counters extends Component {
             key={counter.id}
             onDelete={onDelete}
             onIncrement={onIncrement}
+            onDecrement={onDecrement}
             counter={counter}
           />
         ))}
